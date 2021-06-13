38fa3f550c1d70153cd0c62fb91ebdb46a8f44b5
Prokopenko S.A <serg-prokopenko@mail.ru>
2021-06-13 19:43:48 +0300

    Add foreign key

--- src/main/java/ru/sfedu/BankApp/Main.java
+++ src/main/java/ru/sfedu/BankApp/Main.java
@@ -4,22 +4,21 @@ import ru.sfedu.BankApp.controller.AccountService;
 import ru.sfedu.BankApp.controller.UserService;
 import ru.sfedu.BankApp.models.Account;
 import ru.sfedu.BankApp.models.User;
+import ru.sfedu.BankApp.utils.Util;
 
 import java.sql.*;
 import java.util.Optional;
 
 public class Main {
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserService userService = new UserService();
-        AccountService accountService = new AccountService();
+        Connection connection = new Util().getConnection();
+        UserService userService = new UserService(connection);
+        AccountService accountService = new AccountService(connection);
         //userService.initDB();
         //userService.create(new User("Иван", "Иванов", "Иванович", "м"));
-        //accountService.create(new Account(7382938, 1000, "91ca7f24-e4ce-426b-ad91-3621e5272364"));
-        System.out.println(accountService.getById("34082c67-17c1-4fda-8b67-2b1c799ab58d"));
-        Account account = accountService.getById("34082c67-17c1-4fda-8b67-2b1c799ab58d").get();
-        account.setBalance(2000);
-        System.out.println(accountService.update(account));
-        //System.out.println(userService.getAll().toString());
+        //accountService.create(new Account(7382938, 1000, "60ed851d-7f18-411f-b6aa-0495e9cbfc95"));
+        System.out.println(userService.getAll().toString());
         System.out.println(accountService.getAll().toString());
 
     }
--- src/main/resources/initDB.sql
+++ src/main/resources/initDB.sql
@@ -1,2 +1,2 @@
 create table user ( idUser varchar(40) primary key, name varchar(30) not null, surname varchar(30) not null, patronymic varchar(30), sex varchar(30));
-create table account ( idAccount varchar(40) primary key, number long not null, balance long not null, idUser varchar(40));
+create table account ( idAccount varchar(40) primary key, number long not null, balance long not null, idUser varchar(40), foreign key (idUser) references user(idUser) on delete cascade);
